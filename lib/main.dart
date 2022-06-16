a6c1396bf95e5f6bcd3b772832e6af7b8a90216f
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2022-06-16 15:13:04 +0530

    update: `main.dart`

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:ccd2022app/utils/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:ccd2022app/screens/login_screen.dart';
+import 'package:ccd2022app/screens/registration_screen.dart';
+import 'package:ccd2022app/screens/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,57 +15,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CCD 2022',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: WelcomeScreen.id,
+      theme: lightTheme,
+      routes: {
+        WelcomeScreen.id: (context) => const WelcomeScreen(),
+        LoginScreen.id: (context) => const LoginScreen(),
+        RegistrationScreen.id: (context) => const RegistrationScreen(),
+      },
     );
   }
 }
