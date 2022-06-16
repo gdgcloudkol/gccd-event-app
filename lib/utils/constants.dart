0a328827e4665f97db703d8de10d4eefb899b37e
Srijan Kr. Gupta <gupta.srijan94@gmail.com>
2022-06-16 15:12:00 +0530

    feat: theme

---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const kScaffoldLightBg = Color(0xff1D48D4);
---
+++ lib/utils/theme.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:ccd2022app/utils/constants.dart';
+
+final ThemeData darkTheme = ThemeData.dark().copyWith(
+  scaffoldBackgroundColor: const Color(0xFF212121),
+  textTheme: const TextTheme(
+    bodyText1: TextStyle(color: Color(0x57FFFFFF)),
+    bodyText2: TextStyle(
+      color: Color(0xFFF9F9F9),
+    ),
+  ),
+  inputDecorationTheme: const InputDecorationTheme(
+    border: OutlineInputBorder(),
+    labelStyle: TextStyle(
+      color: Colors.white,
+    ),
+  ),
+);
+
+final ThemeData lightTheme = ThemeData.light().copyWith(
+  scaffoldBackgroundColor: kScaffoldLightBg,
+  textTheme: const TextTheme(
+    bodyText1: TextStyle(color: Color(0xFFF9F9F9)),
+    bodyText2: TextStyle(
+      color: Color(0xFFF9F9F9),
+      // color: Colors.black87,
+    ),
+  ),
+  inputDecorationTheme: const InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderSide: BorderSide(
+        color: Color(0xFFF9F9F9),
+      ),
+    ),
+    labelStyle: TextStyle(
+      color: Color(0xFFF1F1F1),
+    ),
+  ),
+);
