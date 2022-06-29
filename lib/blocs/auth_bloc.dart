a881ef0c556808bec5f881a13ca9c688fc35eac4
Srijan Gupta <gupta.srijan94@gmail.com>
2022-06-29 22:52:59 +0530

    feat: email login and signup

--- lib/blocs/auth_bloc.dart
+++ lib/blocs/auth_bloc.dart
@@ -1,24 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class AuthBloc extends ChangeNotifier{
+class AuthBloc extends ChangeNotifier {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  var errorMessage = '';
+  UserCredential? _credential;
 
-
-  loginWithGoogle(){
+  loginWithGoogle() {
     ///TODO implementation
   }
 
-  loginWithGithub(){
+  loginWithGithub() {
     ///TODO implementation
   }
 
-  loginWithEmailPassword(){
-    ///TODO implementation
+  loginWithEmailPassword(String email, String password) async {
+    try {
+      _credential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      errorMessage = '';
+    } on FirebaseAuthException catch (e) {
+      errorMessage = e.code;
+    } catch (e) {
+      errorMessage = e.toString();
+    } finally {
+      notifyListeners();
+    }
   }
 
-  signUpWithEmailPassword(){
-    ///TODO implementation
+  signUpWithEmailPassword(String email, String password) async {
+    try {
+      _credential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        errorMessage = 'The password is too weak.';
+      } else if (e.code == 'email-already-in-use') {
+        errorMessage = 'The email address is already in use.';
+      } else if (e.code == 'invalid-email') {
+        errorMessage = 'The email address is invalid.';
+      } else {
+        errorMessage = e.code;
+      }
+    } catch (e) {
+      errorMessage = e.toString();
+    } finally {
+      notifyListeners();
+    }
   }
-
-
-
 }
